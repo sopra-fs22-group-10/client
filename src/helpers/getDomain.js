43c34f3b3e911e1abedfa03200f8d29444a4cc3d
mattialeonetti <mattia.leonetti@uzh.ch>
2022-04-13 16:06:47 +0200

    fixed devUrl

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -7,7 +7,7 @@ import { isProduction } from 'helpers/isProduction';
  * @returns {string}
  */
 export const getDomain = () => {
-  const prodUrl = 'https://sopra-fs22-group-10-client.herokuapp.com';
+  const prodUrl = 'https://sopra-fs22-group-10-server.herokuapp.com';
   const devUrl = 'http://localhost:8080';
 
   return isProduction() ? prodUrl : devUrl;
